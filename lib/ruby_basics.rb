dfd8aea9d96234a41031fe47c8402e4ae7f65e2b
Max Eingorn <meingorn@gmail.com>
2017-01-31 15:48:10 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ return num1 / num2
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(x)
+  x
 end
 
-def greeting
-
+def greeting(greeting, name)
+  puts greeting + name
 end
 
 def return_a_value
+  "Nice"
 
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(value = "expert")
+  return value
 end
 
-def pizza_party
-
+def pizza_party(type = "cheese")
+  return type
 end
